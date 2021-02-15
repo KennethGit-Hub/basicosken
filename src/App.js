a3e00d3efa0cb58c8022b5cfe6bb046af542fcb0
KennethGit-Hub <kennethgio12.ktr@gmail.com>
2021-02-15 18:30:10 -0500

    inicio fragment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React {Fragment} from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Fragment } from 'react'
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <Header/>
       <Footer/>
-    </div>
+    </Fragment>
   );
 }
 
